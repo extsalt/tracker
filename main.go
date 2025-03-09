d20ae4fa2ab63e5ea99a1db9f266c70a4ac95ab9
Ganesh Gupta <ganeshk.work@gmail.com>
2025-03-09 19:18:24 +0530

    Add indirect dependencies and update server listening address

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting server on localhost:8000")
 	http.HandleFunc("/c", clickHttpHandler)
-	http.ListenAndServe("localhost:8000", nil)
+	http.ListenAndServe("0.0.0.0:8000", nil)
+
 }
 
 func clickHttpHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +22,16 @@ func clickHttpHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Affiliate ID is missing")
 		return
 	}
-
+	// Get campaigns details from redis and update the click count
+	// Redirect to the campaign URL
+	
+	// Check 1 - Check if the campaign exists
+	// Check 2 - Check if the affiliate exists
+	// Check 3 - Check if the campaign is active
+	// Check 4 - Check if the affiliate is active
+	// Check 5 - Check if the campaign is active for the affiliate
+	// Check 6 - Check if the campaign is active for the affiliate for the current date
+	// Check 7 - Check if the campaign is active for the affiliate for the current date and time
+	// Check 8 - Check if the campaign is active for the affiliate for the current date and time 
+	//
 }
