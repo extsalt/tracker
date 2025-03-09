d55a0b69c89b046595273ddce2f75c6be783b6d6
Ganesh Gupta <ganeshk.work@gmail.com>
2025-03-09 19:24:51 +0530

    Fix server listening address and clean up comments in main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	fmt.Println("Starting server on localhost:8000")
 	http.HandleFunc("/c", clickHttpHandler)
-	http.ListenAndServe("0.0.0.0:8000", nil)
+	http.ListenAndServe(":8000", nil)
 
 }
 
@@ -24,7 +24,7 @@ func clickHttpHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	// Get campaigns details from redis and update the click count
 	// Redirect to the campaign URL
-	
+
 	// Check 1 - Check if the campaign exists
 	// Check 2 - Check if the affiliate exists
 	// Check 3 - Check if the campaign is active
@@ -32,6 +32,6 @@ func clickHttpHandler(w http.ResponseWriter, r *http.Request) {
 	// Check 5 - Check if the campaign is active for the affiliate
 	// Check 6 - Check if the campaign is active for the affiliate for the current date
 	// Check 7 - Check if the campaign is active for the affiliate for the current date and time
-	// Check 8 - Check if the campaign is active for the affiliate for the current date and time 
+	// Check 8 - Check if the campaign is active for the affiliate for the current date and time
 	//
 }
