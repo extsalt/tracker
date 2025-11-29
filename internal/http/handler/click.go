a24d3660395c7b3cdf016184e2617380413cd200
Ganesh Gupta <ganeshk.work@gmail.com>
2025-11-29 16:44:58 +0530

    feat: Add HTTP handler for processing click events and publishing them to Redis Pub/Sub.

--- internal/http/handler/click.go
+++ internal/http/handler/click.go
@@ -1,11 +1,26 @@
 package handler
 
 import (
+	"encoding/json"
 	"extsalt/tracker/internal/pubsub"
+	"slices"
 
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 )
+
+type Offer struct {
+	ID                string   `json:"id"`
+	Name              string   `json:"name"`
+	AllowedPublishers []string `json:"allowed_publishers"`
+}
+
+type ClickPayload struct {
+	OfferID     string `json:"offer_id"`
+	AccountID   string `json:"account_id"`
+	AffiliateID string `json:"affiliate_id"`
+	Status      string `json:"status"`
+}
 
 func HandlerClick(c *gin.Context) {
 	offerID := c.Query("offer_id")
@@ -18,7 +33,7 @@ func HandlerClick(c *gin.Context) {
 		panic(err)
 	}
 
-	_, err = redisClient.Get(c, offerID).Result()
+	val, err := redisClient.Get(c, offerID).Result()
 	if err == redis.Nil {
 		c.JSON(404, gin.H{"error": "offer not found"})
 		return
@@ -26,5 +41,28 @@ func HandlerClick(c *gin.Context) {
 		panic(err)
 	}
 
-	redisClient.Publish(c, "click", "click")
+	var offer Offer
+	if err := json.Unmarshal([]byte(val), &offer); err != nil {
+		panic(err)
+	}
+
+	affiliateID := c.Query("affiliate_id")
+	status := "rejected"
+	if slices.Contains(offer.AllowedPublishers, affiliateID) {
+		status = "approved"
+	}
+
+	payload := ClickPayload{
+		OfferID:     offerID,
+		AccountID:   c.Query("account_id"),
+		AffiliateID: affiliateID,
+		Status:      status,
+	}
+
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	redisClient.Publish(c, "click", payloadBytes)
 }
